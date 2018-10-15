7ee9cde9a5bdca05e8bf5c71f9b38b9a44cebaf6
Scott Wagner <scottywags16@gmail.com>
2018-10-14 22:42:31 -0400

    add eslint and fix existing lint issues

--- src/splitter.js
+++ src/splitter.js
@@ -1,23 +1,21 @@
-import { CUSTOM_PERCENTAGES } from "./splitTypes";
+import { CUSTOM_PERCENTAGES } from './splitTypes';
 
-const handleCustomPercentage = (people, { amount }) => 
-    people.map(person => ({
-        id: person.id,
-        cost: amount * person.share
-    }));
+const handleCustomPercentage = (people, { amount }) => people.map(person => ({
+  id: person.id,
+  cost: amount * person.share,
+}));
 
-const handleEvenly = (people, { amount }) => 
-    people.map(person => ({
-        id: person.id,
-        cost: amount / people.length
-    }));
+const handleEvenly = (people, { amount }) => people.map(person => ({
+  id: person.id,
+  cost: amount / people.length,
+}));
 
 
 const split = (people, cost) => {
-    if(cost.splitType === CUSTOM_PERCENTAGES) {
-        return handleCustomPercentage(people, cost);
-    }
-    return handleEvenly(people, cost);
+  if (cost.splitType === CUSTOM_PERCENTAGES) {
+    return handleCustomPercentage(people, cost);
+  }
+  return handleEvenly(people, cost);
 };
 
 export default split;
